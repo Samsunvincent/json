3c75c7c3da2ad0b83f68e66c995b7f016963ee5c
samsun <samsunvincent143@gmail.com>
2024-08-24 11:33:23 +0530

    first commit

---
+++ client/index.js
@@ -0,0 +1,28 @@
+async function getdata(){
+    try {
+        const datas = await fetch('http://localhost:3000/json')
+        let parsed_datas = await datas.json();
+        console.log("parsed-datas",parsed_datas)
+
+        let datacontainer = document.getElementById("data-container");
+        console.log(datacontainer)
+        
+        let rows = " "
+        for (i = 0;i<parsed_datas.length;i++){
+            rows = rows+`
+          
+            <div>${parsed_datas[i].category}</div>
+            <div>${parsed_datas[i].description}</div>
+
+
+            `
+        }
+        datacontainer.innerHTML = rows;
+        
+
+
+    } catch (error) {
+        console.log("error",error)
+    }
+}
+getdata()
