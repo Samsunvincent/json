cce9731954ca54a618b84eb4a3b74066304a2af2
samsun <samsunvincent143@gmail.com>
2024-08-25 22:39:42 +0530

    added more pages

--- client/index.js
+++ client/index.js
@@ -6,22 +6,19 @@ async function fetchdata(){
 
         let datacontainer = document.getElementById("data-container");
         console.log(datacontainer)
-        
         let rows = " "
         for (i = 0;i<parsed_datas.length;i++){
             rows = rows+`
-          
-            <div>${parsed_datas[i].category}</div>
-            <div>${parsed_datas[i].description}</div>
-
-
+            <div class="shadow p-3 mb-5 bg-body rounded  border border-4  text-center">
+            <div class="p-3"><img src = "${parsed_datas[i].image}" style="width:200px;height:200px;"></div>
+            <div class="fw-bold pt-2">${parsed_datas[i].category}</div>
+            <div class="pt-3">${parsed_datas[i].description.slice(0,95)+".."}</div>
+            </div>
             `
         }
         datacontainer.innerHTML = rows;
-        
-
-
-    } catch (error) {
+    } 
+    catch (error) {
         console.log("error",error)
     }
 }
